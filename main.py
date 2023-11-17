1e223584a59ac88c3a307efd2b1295ae5d32ff5b
Adeeb Khan <94802359+AdeebCanCode@users.noreply.github.com>
2023-11-17 08:00:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI, UploadFile
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
+
+
+
+
 
 app = FastAPI()
 
@@ -43,5 +48,20 @@ async def predict_image(file: UploadFile):
     normal = float(normal)
     
     return {"prediction": prediction, "malignant_prob": malignant, "normal_prob": normal}
+    @app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail},
+    )
+
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # to run the code use this command python -m uvicorn aa:app --reload  
