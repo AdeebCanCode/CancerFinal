afb2f87830c3fad32fcadefdda51d9897c6a3f6e
Adeeb Khan <94802359+AdeebCanCode@users.noreply.github.com>
2023-11-18 05:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,21 @@ from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 import numpy as np
 import tensorflow as tf
-import io  # Add this line to import the io module
+import io
 
 app = FastAPI()
 
-# Lazy loading of the model during startup
-model = None
-
-def load_keras_model():
-    global model
-    if model is None:
-        model = load_model('model_vgg19.h5')
+# Load the model during startup
+model = load_model('model_vgg19.h5')
 
 # Predict function with image resizing and direct processing
 def predict(img_data):
-    load_keras_model()
-    
     img = image.img_to_array(img_data)
     img = np.expand_dims(img, axis=0)
     img_data = preprocess_input(img)
     classes = model.predict(img_data)
     malignant = float(classes[0, 0])  # Convert to float
     normal = float(classes[0, 1])     # Convert to float
-    
     return malignant, normal
 
 @app.post("/predict/")
