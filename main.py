c2a48c6792c4cd1a254104adf4c881d5605863ed
Adeeb Khan <94802359+AdeebCanCode@users.noreply.github.com>
2023-12-01 08:11:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,67 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 import numpy as np
-import tensorflow as tf
-import io
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
-CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-# Load the model during startup
+CORS(app)
+CORS(app, origins='*')
+# Allow all origins for CORS
+
+
+# Load your Keras model
 model = load_model('model_vgg19.h5')
 
-# Predict function with image resizing and direct processing
-def predict(img_data):
-    img = image.img_to_array(img_data)
-    img = np.expand_dims(img, axis=0)
-    img_data = preprocess_input(img)
-    classes = model.predict(img_data)
-    malignant = float(classes[0, 0])  # Convert to float
-    normal = float(classes[0, 1])     # Convert to float
-    return malignant, normal
+def predict(image_path):
+    try:
+        img = image.load_img(image_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        img_data = preprocess_input(x)
+        classes = model.predict(img_data)
+        malignant = float(classes[0, 0])  # Convert to float
+        normal = float(classes[0, 1])     # Convert to float
 
-@app.post("/predict")
-def predict_image(file: UploadFile):
-    # Read image file directly without saving
-    contents = file.file.read()
-    img_data = image.img_to_array(image.load_img(io.BytesIO(contents), target_size=(224, 224)))
-    
-    # Perform prediction on the image data
-    malignant, normal = predict(img_data)
-    
-    # Convert NumPy floats to Python floats
-    malignant = float(malignant)
-    normal = float(normal)
-    
-    if malignant > normal:
-        prediction = 'malignant'
-    else:
-        prediction = 'normal'
-    
-    return {"prediction": prediction, "malignant_prob": malignant, "normal_prob": normal}
+        return malignant, normal
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
+    finally:
+        # Clean up the temporary image file
+        if os.path.exists(image_path):
+            os.remove(image_path)
+
+@app.post("/predict", methods=['POST'])
+async def predict_image(file: UploadFile):
+    temp_image_path = "temp_image.jpg"
+
+    try:
+        # Save the uploaded image temporarily
+        with open(temp_image_path, "wb") as temp_image:
+            temp_image.write(file.file.read())
+
+        # Perform prediction on the saved image
+        malignant, normal = predict(temp_image_path)
+
+        if malignant > normal:
+            prediction = 'malignant'
+        else:
+            prediction = 'normal'
+
+        # Convert NumPy floats to Python floats
+        malignant = float(malignant)
+        normal = float(normal)
+
+        return {"prediction": prediction, "malignant_prob": malignant, "normal_prob": normal}
+    except HTTPException as e:
+        # Re-raise HTTPException to preserve status code and details
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during processing: {str(e)}")
+    finally:
+        # Ensure cleanup even if an exception occurs
+        if os.path.exists(temp_image_path):
+            os.remove(temp_image_path)
